2701346d04546ba790b421365705c88a14697229
anatelli10 <anatelli10@gmail.com>
2021-08-23 04:20:25 -0500

    add types to index exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { TrackingInfo, TrackingEvent } from './util/types';
 export { trackFedex } from './couriers/fedex';
 export { trackUps } from './couriers/ups';
 export { trackUsps } from './couriers/usps';
