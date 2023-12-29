7a917a371637bbe66550e64a481188c1a831e4d9
anatelli10 <anatelli10@gmail.com>
2023-12-28 16:50:13 -0800

    restructure

--- src/couriers/__tests__/utils.test.ts
+++ src/couriers/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import * as utils from '../../src/couriers/utils';
+import * as utils from '../utils';
 
 describe('reverseOneToManyDictionary', () => {
   const input = {
--- src/couriers/utils.ts
+++ src/couriers/utils.ts
@@ -1,26 +1,41 @@
-type OneToManyDictionary = Readonly<Record<string, readonly string[]>>;
-
-// todo:
-// This needs to support throwing TS7053 when using a nonexistent key
-// Currently no type error and type `any` is inferred.
-type ReverseOneToManyDictionary<Dictionary extends OneToManyDictionary> = {
-  [StringArrayValue in Dictionary[keyof Dictionary][number]]: {
-    [Key in keyof Dictionary]-?: StringArrayValue extends Dictionary[Key][number]
+/**
+ * Reverses a Record<string, string[]> such that every value's array member becomes a key pointing to its respective key in the input.
+ * e.g. { animal: ['cat','dog','elephant'] } becomes { cat: 'animal', dog: 'animal', elephant: 'animal' }
+ *
+ * This allows for maps with many keys with the same value to be defined in reverse for better readability & maintainability.
+ *
+ * @example
+ * // `as const` const asserted object input for narrowest typing
+ * const input = {
+ *   LABEL_CREATED: ['PU', 'PX', 'OC'],
+ *   OUT_FOR_DELIVERY: ['OD'],
+ * } as const;
+ *
+ * const output: {
+ *     PU: "LABEL_CREATED";
+ *     PX: "LABEL_CREATED";
+ *     OC: "LABEL_CREATED";
+ *     OD: "OUT_FOR_DELIVERY";
+ * } = reverseOneToManyDictionary(input);
+ *
+ * const value: "LABEL_CREATED" = output['PX'];
+ */
+export const reverseOneToManyDictionary = <
+  OneToManyDictionary extends Readonly<Record<string, readonly string[]>>
+>(
+  oneToManyDictionary: OneToManyDictionary
+): {
+  [StringArrayValue in OneToManyDictionary[keyof OneToManyDictionary][number]]: {
+    readonly [Key in keyof OneToManyDictionary]-?: StringArrayValue extends OneToManyDictionary[Key][number]
       ? Key
       : never;
-  }[keyof Dictionary];
-};
-
-export const reverseOneToManyDictionary = <
-  Dictionary extends OneToManyDictionary
->(
-  oneToManyDictionary: Dictionary
-): ReverseOneToManyDictionary<Dictionary> =>
+  }[keyof OneToManyDictionary];
+} =>
   Object.fromEntries(
     Object.entries(oneToManyDictionary).flatMap(([key, values]) =>
       values.map((value: any) => [value, key])
     )
-  ) as ReverseOneToManyDictionary<Dictionary>;
+  );
 
 export const getLocation = ({
   city,
