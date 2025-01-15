f45111bc303369c83b1fe4d06abbcbdbb40e6b7e
Tony Natelli <tony@natel.li>
2025-01-15 13:48:39 -0800

    chore: tsconfig & use tycelium

--- src/couriers/utils.ts
+++ src/couriers/utils.ts
@@ -1,39 +1,6 @@
 import axios from "axios";
 
-/**
- * Reverses a Record<string, string[]> such that every value's array member becomes a key pointing to its respective key in the input.
- * e.g. { animal: ['cat','dog','elephant'] } becomes { cat: 'animal', dog: 'animal', elephant: 'animal' }
- *
- * This allows for maps with many keys with the same value to be defined in reverse for better readability & maintainability.
- *
- * @example
- * // `as const` const asserted object input for narrowest typing
- * const input = {
- *   LABEL_CREATED: ['PU', 'PX', 'OC'],
- *   OUT_FOR_DELIVERY: ['OD'],
- * } as const;
- *
- * const output: {
- *     PU: "LABEL_CREATED";
- *     PX: "LABEL_CREATED";
- *     OC: "LABEL_CREATED";
- *     OD: "OUT_FOR_DELIVERY";
- * } = reverseOneToManyDictionary(input);
- *
- * const value: "LABEL_CREATED" = output['PX'];
- */
-export const reverseOneToManyDictionary = <OneToManyDictionary extends Readonly<Record<string, readonly string[]>>>(
-  oneToManyDictionary: OneToManyDictionary
-): {
-  [StringArrayValue in OneToManyDictionary[keyof OneToManyDictionary][number]]: {
-    readonly [Key in keyof OneToManyDictionary]-?: StringArrayValue extends OneToManyDictionary[Key][number]
-      ? Key
-      : never;
-  }[keyof OneToManyDictionary];
-} =>
-  Object.fromEntries(
-    Object.entries(oneToManyDictionary).flatMap(([key, values]) => values.map((value: any) => [value, key]))
-  );
+export { reverseOneToManyDictionary } from "tycelium";
 
 export const getLocation = ({
   city,
